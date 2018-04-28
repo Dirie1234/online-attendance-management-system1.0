ec78d14e541e9a5d8097cc7dc6ed88ad447acbb4
A S M Humaun Kabir <psychopath.humaun@gmail.com>
2018-04-28 18:38:37 +0600

    Report Generation and updated :chart_with_upwards_trend:

--- report.php
+++ report.php
@@ -8,15 +8,31 @@ if($_SESSION['name']!='oasis')
   header('location: login.php');
 }
 ?>
-
-<?php  include('connect.php');  ?>
+<?php include('connect.php');?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Online Attendance Management System - City University</title>
+<title>Online Attendance Management System 1.0</title>
 <meta charset="UTF-8">
 <link rel="stylesheet" type="text/css" href="css/main.css">
+<style type="text/css">
+  table th{
+    padding: 7px;
+    border: solid black 4px;
+    font-size: 20px;
+    text-align: center;
+  }
+  table td{
+    border: dotted black 3px;
+    font-size: 20px;
+    text-align: center;
+  }
+  form input{
+    font-size: 20px;
+  }
+
+</style>
 
 </head>
 <body>
@@ -42,11 +58,46 @@ if($_SESSION['name']!='oasis')
 <div class="row">
 
   <div class="content">
-    <p style="font-size: 50px;">Total Students: 3200</p><br>
-    <p style="font-size: 50px;">Total Teachers: 80</p>
+    <h3>Student Report</h3>
     <br>
+    <form method="post" action="">
+      <label>Student ID</label>
+      <input type="text" name="sr_id">
+      <input type="submit" name="sr_btn" value="Done!" >
+    </form>
+    <br>
+    <table border="1">
+      <tr>
+        <th>Student ID</th>
+        <th>Name</th>
+        <th>Department</th>
+        <th>Batch</th>
+        <th>Date</th>
+        <th>Attendance Status</th>
+      </tr>
+   <?php
 
+    if(isset($_POST['sr_btn'])){
+     $sr_id = $_POST['sr_id'];
 
+     $i=0;
+     $all_query = mysql_query("select * from report where report.st_id='$sr_id'");
+     while ($data = mysql_fetch_array($all_query)) {
+       $i++;
+     ?>
+     <tr>
+       <td><?php echo $data['st_id']; ?></td>
+       <td><?php echo $data['st_name']; ?></td>
+       <td><?php echo $data['st_dept']; ?></td>
+       <td><?php echo $data['st_batch']; ?></td>
+       <td><?php echo $data['stat_date']; ?></td>
+       <td><?php echo $data['st_status']; ?></td>
+     </tr>
+     <?php 
+   } 
+     }  
+     ?>
+    </table>
 
   </div>
 
